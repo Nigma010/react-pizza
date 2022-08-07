f8da483ec464d162254eab0dc626cadcb8b70d92
rafa <rafanigmatulin22@gmail.com>
2022-08-07 18:44:42 +0300

    Отрендерил список пицц, список категорий, сделан выбор типа пиццы и размера

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
+import pizzas from "./assets/pizzas.json";
+
 function App() {
   return (
     <div className="wrapper">
@@ -16,7 +18,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock />
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} />
+            ))}
           </div>
         </div>
       </div>
